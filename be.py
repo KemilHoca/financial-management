f9acc2c544a70b6132a8f0d8c71bc065c8aa4490
denizmgirgin-bit <denizmgirgin@gmail.com>
2026-04-07 15:35:29 +0300

    Son

--- be.py
+++ be.py
@@ -28,9 +28,43 @@ class Customer:
             
         self.status_note = ", ".join(notes)
 
+class Add:
+    def __init__(self, customer):
+        self.customer = customer
 
+    def add_customer(self):
+        # Müşteri ekleme işlemi burada yapılır
+        print(f"{self.customer.name} adlı müşteri eklendi.")
 
+class Service:
+    def __init__(self, customer):
+        self.customer = customer
 
+    def provide_service(self):
+        # Hizmet sağlama işlemi burada yapılır
+        print(f"{self.customer.name} adlı müşteriye hizmet sağlanıyor.")
+
+class Validate:
+    def __init__(self, customer):
+        self.customer = customer
+
+        def validate_customer(self):
+            # Müşteri doğrulama işlemi burada yapılır
+            if self.customer.risk_level == "Yüksek Risk":
+                print(f"{self.customer.name} adlı müşteri yüksek riskli olarak işaretlendi.")
+            else:
+                print(f"{self.customer.name} adlı müşteri doğrulandı.")
+
+class İnput:
+    def __init__(self, name, company, balance, delay_days, active_projects):
+        self.name = name
+        self.company = company
+        self.balance = balance
+        self.delay_days = delay_days
+        self.active_projects = active_projects
+
+    def get_customer_data(self):
+        return Customer(self.name, self.company, self.balance, self.delay_days, self.active_projects)
 
 
 
